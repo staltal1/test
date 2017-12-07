1874d15ae709ce77f742448ac9f42f1708ebb316
staltal1 <34313841+staltal1@users.noreply.github.com>
2017-12-07 11:10:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var express = require('express')
 var mongoose = require('mongoose');
 var app = express()
-
+var result = "";
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+var uristring = 'mongodb://localhost';
 
 app.get('/', function(request, response) {
+  response.send(result);
+});
   
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
   
-  
-var uristring = 'mongodb://localhost';
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-  response.send('error');
+  result = "error"; 
   } else {
-  response.send('good');
+  result = "good";
   }
 });
-
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
