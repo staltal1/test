cd5c89b7b63877286d6db0161c734a8c5145e660
staltal1 <34313841+staltal1@users.noreply.github.com>
2017-12-07 11:05:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+var uristring = 'mongodb://localhost';
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
