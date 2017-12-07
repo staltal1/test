1cd0bc316e90262c91dcb9e7deee9811282a8b0f
staltal1 <34313841+staltal1@users.noreply.github.com>
2017-12-07 13:01:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var mongoose = require('mongoose');
 var app = express()
 var result = "";
-
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 var uristring = 'mongodb://db/local';
 
